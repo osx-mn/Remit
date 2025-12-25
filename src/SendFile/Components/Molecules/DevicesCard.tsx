67ac97d0386af2751be8c4e59d080b2bbb88f2b2
osxkar <133513764+osx-mn@users.noreply.github.com>
2025-12-25 12:28:14 -0500

    test de print por ip

--- src/SendFile/Components/Molecules/DevicesCard.tsx
+++ src/SendFile/Components/Molecules/DevicesCard.tsx
@@ -1,14 +1,25 @@
+
 import DynamicTitle from "../Atoms/DynamicTitle";
 
-interface DevicesCardProps{
-    username: String;
+interface Dispositivo {
+    full_name: string;
+    disp_name: string;
+    ip: string;
+    port: number;
+    properties: Array<[String, String]>;
 }
 
-const DevicesCard: React.FC<DevicesCardProps> = ({ username }) => {
+interface DevicesCardProps{
+    deviceProps: Dispositivo;
+    getDeviceIp: (ip: string) => void;
+}
+
+const DevicesCard: React.FC<DevicesCardProps> = ({ deviceProps, getDeviceIp }) => {
     return(
-        <div className="w-[90%] h-[40px] bg-[#303030] rounded-[5px] mt-[10px] center-v">
-           <DynamicTitle title={username}/>
-        </div>
+        <button className="w-[90%] h-[40px] bg-[#303030] rounded-[5px] mt-[10px] center-v"
+        onClick={() => getDeviceIp(deviceProps.ip)}>
+           <DynamicTitle title={deviceProps.properties[0][1]}/>
+        </button>
     )
 }
 
--- src/SendFile/Components/Organisms/DevicesContainer.tsx
+++ src/SendFile/Components/Organisms/DevicesContainer.tsx
@@ -1,4 +1,3 @@
-
 import DevicesCard from "../Molecules/DevicesCard";
 
 interface Dispositivo {
@@ -14,11 +13,16 @@ interface DevicesContainerProps {
 }
 
 const DevicesContainer: React.FC<DevicesContainerProps> = ({devicesList}) =>{
+
+    const getDeviceIp = (ip: string) => {
+        console.log("ip presionado: ", ip);
+    }
+
     return(
         <div className="flex flex-col items-center w-[250px] h-[98%] bg-[#252525] rounded-[5px] ml-[5px]">
             <p className="text-white text-center text-[24px]">Dispositivos</p>
             {devicesList.map((device, _) => {
-                return <DevicesCard key={device.full_name} username={device.properties[0][1]}/>
+                return <DevicesCard key={device.full_name} deviceProps={device} getDeviceIp={getDeviceIp}/>
             })}
         </div>
     )
